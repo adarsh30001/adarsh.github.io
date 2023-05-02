be9bfec57d3e30942741ea220ce7596d13faea64
adarsh30001 <83150499+adarsh30001@users.noreply.github.com>
2023-05-02 09:11:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+  if(mySrc === 'firefox.png') {
+    myImage.setAttribute ('src','firefox2.png');
   } else {
-    myImage.setAttribute ('src','images/firefox.png');
+    myImage.setAttribute ('src','firefox.png');
   }
 }
 
